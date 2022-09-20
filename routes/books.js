5680f4bbc68c848a42be0eb5a1d67cf7719dfab4
Eleos <Peaceaanu247@gmail.com>
2022-09-20 16:52:42 +0100

    ACL Auth for createBook done

--- routes/books.js
+++ routes/books.js
@@ -7,10 +7,35 @@ const fsPromises = require('fs').promises
 const path = require('path')
 
 const booksDbPath = path.join(__dirname, '..', 'db', 'books.json')
+const usersDbPath = path.join(__dirname, '..', 'db', 'users.json')
 
+
+booksRoutes.use(async (req, res, next) => {
+    const loginDetails = req.body.user
+    const role = ['admin']
+
+    if (!loginDetails) {
+        return res.status(403).json({ error: "No credentials sent" })
+    }
+
+    let allUsers = await fsPromises.readFile(usersDbPath)
+    allUsers = JSON.parse(allUsers)
+
+    const userFound = allUsers.find(user => user.username === loginDetails.username && user.password === loginDetails.password)
+
+    if (!userFound) {
+        return res.status(403).json({ error: "Invalid Username or Password" })
+    }
+
+    if (!role.includes(userFound.role)) {
+        return res.status(403).json({ error: "You do not have required access to perform this operation" })
+    }
+
+    next()
+})
 
 booksRoutes.post('/createBook', async (req, res) => {
-    const newBook = req.body
+    const newBook = req.body.book
 
     let allBooks = await fsPromises.readFile(booksDbPath)
 
