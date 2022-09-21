ab3db4d93a3c4cd950473920885e1bcb06b9ce43
Eleos <Peaceaanu247@gmail.com>
2022-09-21 11:12:29 +0100

    book routes alomst done

---
+++ middleware/books.middleware.js
@@ -0,0 +1,80 @@
+
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const booksDbPath = path.join(__dirname, '..', 'db', 'books.json')
+const usersDbPath = path.join(__dirname, '..', 'db', 'users.json')
+
+
+async function passwordAuthMiddleware(req, res, next) {
+    const role = ['admin']
+
+    const loginDetails = req.body.user
+
+    if (!loginDetails) {
+        return res.status(403).json({ error: "No credentials sent" })
+    }
+
+    let allUsers = await fsPromises.readFile(usersDbPath)
+    allUsers = JSON.parse(allUsers)
+
+    const userFound = allUsers.find(user => user.username === loginDetails.username && user.password === loginDetails.password)
+
+    if (!userFound) {
+        return res.status(403).json({ error: "Invalid Username or Password" })
+    }
+
+    next()
+}
+
+async function adminAuthMiddleware(req, res, next) {
+    const role = ["admin"]
+
+    const loginDetails = req.body.user
+
+    if (!loginDetails) {
+        return res.status(403).json({ error: "No credentials sent" })
+    }
+
+    let allUsers = await fsPromises.readFile(usersDbPath)
+    allUsers = JSON.parse(allUsers)
+
+    const userFound = allUsers.find(user => user.username === loginDetails.username && user.password === loginDetails.password)
+
+    if (!userFound) {
+        return res.status(403).json({ error: "Invalid Username or Password" })
+    }
+
+    if (!role.includes(userFound.role)) {
+        return res.status(403).json({ error: "You do not have required access to perform this operation" })
+    }
+
+    next()
+}
+
+async function visitorAuthMiddleware(req, res, next) {
+    const role = ["visitor"]
+
+    const loginDetails = req.body.user
+
+    if (!loginDetails) {
+        return res.status(403).json({ error: "No credentials sent" })
+    }
+
+    let allUsers = await fsPromises.readFile(usersDbPath)
+    allUsers = JSON.parse(allUsers)
+
+    const userFound = allUsers.find(user => user.username === loginDetails.username && user.password === loginDetails.password)
+
+    if (!userFound) {
+        return res.status(403).json({ error: "Invalid Username or Password" })
+    }
+
+    if (!role.includes(userFound.role)) {
+        return res.status(403).json({ error: "You do not have required access to perform this operation" })
+    }
+
+    next()
+}
+
+module.exports = { passwordAuthMiddleware, adminAuthMiddleware, visitorAuthMiddleware } 
--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,6 @@
 const express = require('express')
 
 const booksRoutes = express.Router()
-const createBook = require('../required_files/requestHandler')
 
 const fsPromises = require('fs').promises
 const path = require('path')
@@ -9,32 +8,11 @@ const path = require('path')
 const booksDbPath = path.join(__dirname, '..', 'db', 'books.json')
 const usersDbPath = path.join(__dirname, '..', 'db', 'users.json')
 
+// ------------ Import the Authentication Middleware ----------
+const { passwordAuthMiddleware, adminAuthMiddleware, visitorAuthMiddleware } = require('../middleware/books.middleware')
 
-booksRoutes.use(async (req, res, next) => {
-    const loginDetails = req.body.user
-    const role = ['admin']
-
-    if (!loginDetails) {
-        return res.status(403).json({ error: "No credentials sent" })
-    }
-
-    let allUsers = await fsPromises.readFile(usersDbPath)
-    allUsers = JSON.parse(allUsers)
-
-    const userFound = allUsers.find(user => user.username === loginDetails.username && user.password === loginDetails.password)
-
-    if (!userFound) {
-        return res.status(403).json({ error: "Invalid Username or Password" })
-    }
-
-    if (!role.includes(userFound.role)) {
-        return res.status(403).json({ error: "You do not have required access to perform this operation" })
-    }
-
-    next()
-})
-
-booksRoutes.post('/createBook', async (req, res) => {
+// ------------ Create Books route --------------
+booksRoutes.post('/create', [passwordAuthMiddleware, adminAuthMiddleware], async (req, res) => {
     const newBook = req.body.book
 
     let allBooks = await fsPromises.readFile(booksDbPath)
@@ -51,20 +29,68 @@ booksRoutes.post('/createBook', async (req, res) => {
 
 })
 
-booksRoutes.delete('/deleteBook', async (req, res) => {
-    const bookId = req.body.book
+// ------------ Delete Books Route ----------------
+booksRoutes.delete('/delete', [passwordAuthMiddleware, adminAuthMiddleware], async (req, res) => {
+    const bookId = req.body.book.id
 
     let allBooks = await fsPromises.readFile(booksDbPath)
     allBooks = JSON.parse(allBooks)
 
     const bookToDelete = allBooks.findIndex(book => book.id === bookId)
 
-    allBooks.splice(bookToDelete, 1)
+    if (bookToDelete === -1) {
+        return res.status(400).send("Book not found")
+    }
 
-    updatedBooks = await fsPromises.writeFile(booksDbPath, JSON.stringify(allBooks))
+    allBooks.splice(bookToDelete, 1)
+    fsPromises.writeFile(booksDbPath, JSON.stringify(allBooks))
+
+    lastBookId = allBooks[allBooks.length - 1].id
 
     res.status(200).send("Book deleted successfully")
 })
 
+// ------------ Loan Books Route ----------------
+booksRoutes.post('/loan', [passwordAuthMiddleware, visitorAuthMiddleware], async (req, res) => {
+    const loanBook = req.body.book
+
+    let allBooks = await fsPromises.readFile(booksDbPath)
+    allBooks = JSON.parse(allBooks)
+
+    const bookFound = allBooks.findIndex(book => book.title === loanBook.title)
+
+    if (bookFound === -1) {
+        res.status(404).send('The Book you want to loan is not available')
+    }
+
+    allBooks.splice(bookFound, 1)
+    fsPromises.writeFile(booksDbPath, JSON.stringify(allBooks))
+
+    res.status(200).json({ success: `${loanBook.title} loaned out successfully` })
+})
+
+// ------------ Return Books Route ----------------
+booksRoutes.post('/return', [passwordAuthMiddleware, visitorAuthMiddleware], async (req, res) => {
+    const bookReturned = req.body.book
+
+    let allBooks = await fsPromises.readFile(booksDbPath)
+    allBooks = JSON.parse(allBooks)
+
+    const bookFound = allBooks.find(book => book.title === bookReturned.title)
+ 
+    if (bookFound) {
+        res.writeHead(500).send("You have already returned this book")
+    } else {
+        let lastBookId = allBooks[allBooks.length - 1].id
+        bookReturned.id = lastBookId + 1
+
+        let updateBooks = [...allBooks, bookReturned] 
+
+        fsPromises.writeFile(booksDbPath, JSON.stringify(updateBooks))
+
+        res.status(200).send("Book returned successfully")
+    }
+
+})
 
 module.exports = booksRoutes;
